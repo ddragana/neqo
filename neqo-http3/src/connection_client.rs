f94aed1176f2633d39ef44897e244d0ce597d8c7
Dragana Damjanovic <dragana.damjano@gmail.com>
2021-05-29 00:01:03 +0200

    Crate trait SendStream that will be use by http request/response streams and webTransport streams

--- neqo-http3/src/connection_client.rs
+++ neqo-http3/src/connection_client.rs
@@ -305,7 +305,12 @@ impl Http3Client {
 
         self.base_handler.add_streams(
             id,
-            SendMessage::new_with_headers(id, final_headers, Box::new(self.events.clone())),
+            Box::new(SendMessage::new_with_headers(
+                id,
+                final_headers,
+                self.base_handler.qpack_encoder.clone(),
+                Box::new(self.events.clone()),
+            )),
             Box::new(RecvMessage::new(
                 MessageType::Response,
                 id,
@@ -322,10 +327,7 @@ impl Http3Client {
             .send_streams
             .get_mut(&id)
             .ok_or(Error::InvalidStreamId)?
-            .send(
-                &mut self.conn,
-                &mut self.base_handler.qpack_encoder.borrow_mut(),
-            );
+            .send(&mut self.conn);
         if let Err(e) = output {
             if e.connection_error() {
                 self.close(now, e.code(), "");
